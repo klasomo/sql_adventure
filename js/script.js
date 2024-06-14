a57c446cc5c111602e54cb0f71025ae2d7490fee
klasomo <59315799+klasomo@users.noreply.github.com>
2024-06-14 13:23:48 +0200

    change bomb view background image

--- js/script.js
+++ js/script.js
@@ -245,7 +245,7 @@ function handleButtonClick(viewIndex, dbName, backgroundImage) {
   }
 
   function loadBombView(){
-    handleButtonClick(3, "bomb","./assets/images/background/bombWorkbench.png");
+    handleButtonClick(3, "bomb","./assets/images/background/bomb.png");
   }
   
   // Example of opening databases

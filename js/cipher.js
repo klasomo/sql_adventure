e764c0f4a06f76d610b06b07d9d93511594ee1db
klasomo <59315799+klasomo@users.noreply.github.com>
2024-06-20 17:03:20 +0200

    step outlines created

--- js/cipher.js
+++ js/cipher.js
@@ -1,41 +1,43 @@
-const t = document.querySelector('.codedText');
-const decryptBtn = document.getElementById("decryptBtn");
-const decrptedMesg = "Diese Nachricht wurde Erfolgreich Entschlüsselt"
+function initDecryption(){
+  const t = document.querySelector('.codedText');
+  const decryptBtn = document.getElementById("decryptBtn");
+  const decrptedMesg = "Diese Nachricht wurde Erfolgreich Entschlüsselt"
 
-decryptBtn.addEventListener("click", () => {
-  if (t) {
-    const arr1 = t.value.split('');
-    console.log(arr1);
-    const arr2 = [];
-    arr1.forEach((char, i) => arr2[i] = randChar()); // fill arr2 with random characters
+  decryptBtn.addEventListener("click", () => {
+    if (t) {
+      const arr1 = t.value.split('');
+      console.log(arr1);
+      const arr2 = [];
+      arr1.forEach((char, i) => arr2[i] = randChar()); // fill arr2 with random characters
 
-    const tl = gsap.timeline();
-    let step = 0;
-    tl.fromTo(t, {
-      innerHTML: arr2.join(''),
-    }, {
-      duration: arr1.length / 7, // duration based on text length
-      ease: 'power4.in',
-      onUpdate: () => {
-        const p = Math.floor(tl.progress() * (arr1.length)); // whole number from 0 - text length
-        if (step != p) { // throttle the change of random characters
-          step = p;
-          arr1.forEach((char, i) => arr2[i] = randChar());
-          let pt1 = arr1.join('').substring(0, p),
-              pt2 = arr2.join('').substring(0, arr2.length - p);
-          if (t.classList.contains('fromRight')) {
-            pt1 = arr2.join('').substring(0, arr2.length - p);
-            pt2 = arr1.join('').substring(0, arr1.length - p);
+      const tl = gsap.timeline();
+      let step = 0;
+      tl.fromTo(t, {
+        innerHTML: arr2.join(''),
+      }, {
+        duration: arr1.length / 7, // duration based on text length
+        ease: 'power4.in',
+        onUpdate: () => {
+          const p = Math.floor(tl.progress() * (arr1.length)); // whole number from 0 - text length
+          if (step != p) { // throttle the change of random characters
+            step = p;
+            arr1.forEach((char, i) => arr2[i] = randChar());
+            let pt1 = arr1.join('').substring(0, p),
+                pt2 = arr2.join('').substring(0, arr2.length - p);
+            if (t.classList.contains('fromRight')) {
+              pt1 = arr2.join('').substring(0, arr2.length - p);
+              pt2 = arr1.join('').substring(0, arr1.length - p);
+            }
+            t.value = pt1 + pt2; // update text
           }
-          t.value = pt1 + pt2; // update text
         }
-      }
-    });
-  }
-});
+      });
+    }
+  });
 
-function randChar() {
-  let c = "abcdefghijklmnopqrstuvwxyz1234567890!@#$^&*()…æ_+-=;[]/~`";
-  c = c[Math.floor(Math.random() * c.length)];
-  return (Math.random() > 0.5) ? c : c.toUpperCase();
+  function randChar() {
+    let c = "abcdefghijklmnopqrstuvwxyz1234567890!@#$^&*()…æ_+-=;[]/~`";
+    c = c[Math.floor(Math.random() * c.length)];
+    return (Math.random() > 0.5) ? c : c.toUpperCase();
+  }
 }
--- js/stepManager.js
+++ js/stepManager.js
@@ -1,11 +1,11 @@
 
-let currentStep = 6;
+let currentStep = 4;
 
 const stepsNames = [
-    { id: 'step1', message: 'Step1' },
-    { id: 'step2', message: 'Step2' },
-    { id: 'step3', message: 'Step3' },
-    { id: 'step4', message: 'Step4' },
+    { id: 'step1', message: 'Fall Übersicht' },
+    { id: 'step2', message: 'Am PC anmelden' },
+    { id: 'step3', message: 'Täter' },
+    { id: 'step4', message: 'Zugangsberechtigung erhöhen' },
     { id: 'step5', message: 'Step5' },
     { id: 'step6', message: 'Standort Lokalisieren' },
     { id: 'step7', message: 'Bombe entschärfen' },
@@ -86,10 +86,9 @@ function setStepClickEvent(){
                 
                 modalContent.style.display = 'block';
                 modalContent.querySelector('div').style.display = 'block';;
-                if(currentStep + 1 == stepsNames.length){
-                    console.log("init Bomb color picker");
-                    initColorPicker();
-                }
+
+                initStep();
+
                 console.log(modalContent);
             });
 
@@ -100,5 +99,28 @@ function setStepClickEvent(){
     });
 }
 
+function initStep(){
+    switch(currentStep){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            initDecryption();
+            break;
+        case 5:
+            break;
+        case 6:
+            initColorPicker();
+            break;
+        default:
+            break;
+    }
+}
 
 // Step 1:
+function checkStepInput(){
+    
+}

8c92c9a0c0407ada833b1863e20252c17131b44d
klasomo <59315799+klasomo@users.noreply.github.com>
2024-06-24 15:48:39 +0200

    add Timer function for pause, add, continiue

--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,40 @@
 let startTime;
+let pausedTime = 0;
+let isPaused = false;
+let intervalId;
 const timerDisplay = document.querySelector('.timer');
 
 function startTimer() {
     startTime = Date.now();
-    setInterval(updateTimer, 1000);
+    intervalId = setInterval(updateTimer, 1000);
 }
 
 function updateTimer() {
-    const currentTime = new Date(Date.now() - startTime);
+    const currentTime = new Date(Date.now() - startTime + pausedTime);
     const minutes = Math.floor(currentTime / (1000 * 60)).toString().padStart(2, '0');
     const seconds = (Math.floor(currentTime / 1000) % 60).toString().padStart(2, '0');
     timerDisplay.textContent = `${minutes}:${seconds}`;
 }
 
+function addTime(secondsToAdd) {
+    pausedTime += secondsToAdd * 1000;
+}
+
+function pauseTimer() {
+    if (!isPaused) {
+        isPaused = true;
+        pausedTime += Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+}
+
+function continueTimer() {
+    if (isPaused) {
+        isPaused = false;
+        startTime = Date.now();
+        intervalId = setInterval(updateTimer, 1000);
+    }
+}
+
 window.addEventListener('load', startTimer);
+
